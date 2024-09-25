ea069bce6dae54084737c80fc6f62b0904874039
weakinyi <akinyiwendy46@gmail.com>
2024-09-25 09:28:50 +0300

    feat: add file correlation.go

---
+++ statistics/correlation.go
@@ -0,0 +1,13 @@
+package statistics
+
+import "math"
+
+func Correlation(x, y []float64, xMean, yMean float64) float64 {
+	var num, denomX, denomY float64
+	for i := 0; i < len(x); i++ {
+		num += (x[i] - xMean) * (y[i] - yMean)
+		denomX += (x[i] - xMean) * (x[i] - xMean)
+		denomY += (y[i] - yMean) * (y[i] - yMean)
+	}
+	return num / math.Sqrt(denomX*denomY)
+}
